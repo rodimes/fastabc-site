034e2c9186d217c28626175dafad3e551f788dd5
Rodrigo Santos <rodrigo@improving.com.br>
2018-04-09 21:59:47 -0300

    Commit contendo configurações iniciais do site fastabc com spring cloud

---
+++ site-service/src/main/java/br/com/fastabc/SiteServiceApplication.java
@@ -0,0 +1,75 @@
+package br.com.fastabc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@SpringBootApplication
+@EnableEurekaClient
+@RestController
+@RequestMapping("/site")
+public class SiteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteServiceApplication.class, args);
+	}
+
+	private List<Book> bookList = Arrays.asList(
+			new Book(1L, "Teste", "Tim Schimandle"),
+			new Book(2L, "Baeldung goes to the park", "Slavisa")
+	);
+
+
+	@GetMapping("")
+	public List<Book> findAllBooks() {
+		return bookList;
+	}
+
+	@GetMapping("/{bookId}")
+	public Book findBook(@PathVariable Long bookId) {
+		return bookList.stream().filter(b -> b.getId().equals(bookId)).findFirst().orElse(null);
+	}
+
+	class Book {
+		private Long id;
+		private String author;
+		private String title;
+
+		public Book(Long id, String author, String title) {
+			this.id = id;
+			this.author = author;
+			this.title = title;
+		}
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getAuthor() {
+			return author;
+		}
+
+		public void setAuthor(String author) {
+			this.author = author;
+		}
+
+		public String getTitle() {
+			return title;
+		}
+
+		public void setTitle(String title) {
+			this.title = title;
+		}
+	}
+}
